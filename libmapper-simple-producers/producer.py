79a30c7188a8d546443d4e4f7df448642cc8f4ab
Matt Peachey <peacheym@dal.ca>
2021-09-09 14:06:07 -0300

    Add simple 0<->1 producer.

---
+++ libmapper-simple-producers/producer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import mapper as mpr
+
+# Set up libmapper objects.
+dev = mpr.device("tkgui")
+sig_out = dev.add_signal(mpr.DIR_OUT, "output", 1, mpr.FLT, None, 0, 100)
+
+# Set up counter variables
+new_value = 0.0
+direction = 1
+
+while(True):
+    dev.poll(100)
+    
+    new_value = round(new_value + 0.1 * direction, 1)
+    
+    # Ensure that the value is always between 0 and 1 inclusive
+    if new_value == 0.0 or new_value == 1.0:
+        direction *= -1
+    
+    print(new_value)
+    sig_out.set_value(new_value)
