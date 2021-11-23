dab6ac37da8e9ff44eaacb2c54514afc16d3811e
Matt Peachey <peacheym@dal.ca>
2021-11-23 13:52:54 -0400

    Add some librealsense examples to this sandbox.

---
+++ realsense/realsense-libmapper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import pyrealsense2 as rs
+
+
+pipe = rs.pipeline()
+profile = pipe.start()
+
+try:
+  for i in range(0, 100):
+    frames = pipe.wait_for_frames()
+    for f in frames:
+      print(f.profile)
+      exit()
+finally:
+    pipe.stop()
+    
