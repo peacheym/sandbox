e45e59bed2efefadf4f9b1d69faad5cd594e931a
Matt Peachey <peacheym@dal.ca>
2021-11-24 09:25:23 -0400

    Use realsense color frame as input to mediapipe.

--- realsense/mediapipe_depth.py
+++ realsense/mediapipe_depth.py
@@ -10,6 +10,10 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import mediapipe as mp
+# mp_drawing = mp.solutions.drawing_utils
+# mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
@@ -41,38 +45,48 @@ else:
 pipeline.start(config)
 
 try:
-    while True:
+    with mp_hands.Hands(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as hands:
 
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
+        while True:
 
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
+            # Wait for a coherent pair of frames: depth and color
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue
 
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(
-            depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            # Convert images to numpy arrays
+            # depth_image = np.asanyarray(depth_frame.get_data()) # REMOVE FOR SPEED
+            color_image = np.asanyarray(color_frame.get_data())
 
-        depth_colormap_dim = depth_colormap.shape
-        color_colormap_dim = color_image.shape
+            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+            # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(
+            #     depth_image, alpha=0.03), cv2.COLORMAP_JET) # REMOVE FOR SPEED
 
-        # If depth and color resolutions are different, resize color image to match depth image for display
-        if depth_colormap_dim != color_colormap_dim:
-            resized_color_image = cv2.resize(color_image, dsize=(
-                depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-            images = np.hstack((resized_color_image, depth_colormap))
-        else:
-            images = np.hstack((color_image, depth_colormap))
+            # depth_colormap_dim = depth_colormap.shape # REMOVE FOR SPEED
+            color_colormap_dim = color_image.shape
 
-        # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        cv2.waitKey(1)
+            # If depth and color resolutions are different, resize color image to match depth image for display
+            # if depth_colormap_dim != color_colormap_dim:
+            #     resized_color_image = cv2.resize(color_image, dsize=(
+            #         depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+            #     images = np.hstack((resized_color_image, depth_colormap))
+            # else:
+            #     images = np.hstack((color_image, depth_colormap)) # REMOVE FOR SPEED
+
+            # Run MEDIAPIPE here.
+            results = hands.process(color_image)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    print(hand_landmarks)
+
+            # Show images
+            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', color_image)
+            cv2.waitKey(1)
 
 finally:
 
